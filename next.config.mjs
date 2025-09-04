eb23845853a8521887ab6655f813423076f650a8
xxdoopie <89516563+xxdoopie@users.noreply.github.com>
2025-09-04 15:08:54 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+    // Force Next.js to use PostCSS instead of lightningcss
+    optimizeCss: false,
+  },
+  images: {
+    domains: ['res.cloudinary.com', 'images.unsplash.com'],
+  },
+};
 
 export default nextConfig;
